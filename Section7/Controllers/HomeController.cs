a0d0c96e3d15f7e9f19a431bb037c5b47ec4478d
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-03-28 13:28:11 +0200

    Add project files.

---
+++ Section7/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Section7.Models;
+
+namespace Section7.Controllers;
+public class HomeController : Controller
+{
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return Content("<h1>Welcome to the Section 7 - Person Area.</h1>", "text/html");
+    }
+
+    [Route("register")]
+    public IActionResult Index(Person person)
+    {
+        if (!ModelState.IsValid)
+        {
+            string errors = string.Join("\n",
+            ModelState.Values.SelectMany(value => value.Errors).Select(err =>
+            err.ErrorMessage));
+
+            return BadRequest(errors);
+        }
+
+        return Content($"{person}");
+    }
+}
---
+++ Section7/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Section7.Models;
+public class Person
+{
+    [Required(ErrorMessage ="{0} can not be empty or null.")]
+    [Display (Name ="Person Name")]
+    [StringLength (50, MinimumLength = 3, ErrorMessage ="{0} should be between {2} and " +
+        "{1} characters long")]
+
+    public string? PersonName { get; set; } /*= "Jack";*/
+    public string? Email { get; set; } = "jack@jack.com";
+    public string? Phone { get; set; } = "46-83-83-234";
+    public string? Password { get; set; } = "0";
+    public string? ConfirmPassword { get; set; } = "0";
+
+    [Range (0,999.99, ErrorMessage = "{0} should be between ${1} and ${2}")]
+    public double? Price { get; set; } = 22323232;
+    public override string ToString()
+    {
+        return $"Person name: {PersonName}, \nEmail      : {Email}, \nPhone      : {Phone}, \n" +
+          $"Password   : {Password}, \nConfirm Password: {ConfirmPassword}, \nPrice      : {Price}\n";
+    }
+}
+
+
