e3cbbd18c4bddcb1266b3d97aad6d7026864caef
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-03-29 10:29:10 +0200

    CustomValidators

---
+++ Section7/CustomValidators/MinimumYearValidatorAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Section7.CustomValidators;
+public class MinimumYearValidatorAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        string ErrorMessage = "Minimum year allowed is 2000";
+        if (value != null) 
+        {
+            DateTime date = (DateTime)value;
+            if (date.Year >= 2000)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+            return ValidationResult.Success;
+        }
+        return null;
+    }
+}
+
--- Section7/Models/Person.cs
+++ Section7/Models/Person.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿
+using Section7.CustomValidators;
+using System.ComponentModel.DataAnnotations;
 
 namespace Section7.Models;
 public class Person
@@ -7,15 +9,38 @@ public class Person
     [Display (Name ="Person Name")]
     [StringLength (50, MinimumLength = 3, ErrorMessage ="{0} should be between {2} and " +
         "{1} characters long")]
-
+    [RegularExpression("^[A-Za-z .]$", ErrorMessage ="{0} should contain only alphabets," +
+        "space and dot (.)")]
     public string? PersonName { get; set; } /*= "Jack";*/
+
+
+    [EmailAddress(ErrorMessage = "{0} should be proper email address." )]
+    [Required(ErrorMessage = "{0} can't be blank")]
     public string? Email { get; set; } = "jack@jack.com";
+
+
+    [Phone(ErrorMessage ="{0} should contain 10 digits.")]
+    //[ValidateNever]
     public string? Phone { get; set; } = "46-83-83-234";
+
+
+    [Required(ErrorMessage ="{0} can't be blank")]
     public string? Password { get; set; } = "0";
+
+
+    [Required(ErrorMessage = "{0} can't be blank")]
+    [Compare("Password", ErrorMessage ="{0} and {1} do not match.")]
+    [Display(Name = "Re-enter Password")]
     public string? ConfirmPassword { get; set; } = "0";
+
 
     [Range (0,999.99, ErrorMessage = "{0} should be between ${1} and ${2}")]
     public double? Price { get; set; } = 22323232;
+
+
+    [MinimumYearValidator]
+    public DateTime DateOfBirth { get; set; }
+
     public override string ToString()
     {
         return $"Person name: {PersonName}, \nEmail      : {Email}, \nPhone      : {Phone}, \n" +
